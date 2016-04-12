91dba47e5528f3ac29a111598fbad8a36aa468cd
Arkadiusz Konior <arkadiusz.konior@itako.pl>
2016-04-12 14:21:20 +0200

    usoses list

--- app/src/main/java/mobi/kujon/KujonApplication.java
+++ app/src/main/java/mobi/kujon/KujonApplication.java
@@ -8,10 +8,16 @@ import io.fabric.sdk.android.Fabric;
 
 public class KujonApplication extends Application {
 
+    private static Application instance;
+
     @Override public void onCreate() {
         super.onCreate();
+        instance = this;
         Fabric.with(this, new Crashlytics());
 //         Fabric.with(this, new Crashlytics.Builder().core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()).build());
     }
 
+    public static Application getContext() {
+        return instance;
+    }
 }
--- app/src/main/java/mobi/kujon/activities/UsosesActivity.java
+++ app/src/main/java/mobi/kujon/activities/UsosesActivity.java
@@ -2,7 +2,18 @@ package mobi.kujon.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -16,9 +27,12 @@ import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+import static mobi.kujon.KujonApplication.getContext;
+
 public class UsosesActivity extends BaseActivity {
 
     @Bind(R.id.recyclerView) RecyclerView recyclerView;
+    private UsosesAdapter adapter;
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +40,10 @@ public class UsosesActivity extends BaseActivity {
         ButterKnife.bind(this);
 
         requestUsoses();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        adapter = new UsosesAdapter();
+        recyclerView.setAdapter(adapter);
     }
 
     private void requestUsoses() {
@@ -39,7 +57,8 @@ public class UsosesActivity extends BaseActivity {
         kujonBackendService.usoses().enqueue(new Callback<UsosesResponse>() {
             @Override public void onResponse(Call<UsosesResponse> call, Response<UsosesResponse> response) {
                 System.out.println(response);
-                for (Usos usos : response.body().data) {
+                adapter.setItems(response.body().data);
+                for (mobi.kujon.network.json.Usos usos : response.body().data) {
                     System.out.println(usos);
                 }
             }
@@ -48,5 +67,43 @@ public class UsosesActivity extends BaseActivity {
                 System.out.println(t);
             }
         });
+    }
+
+    private static class UsosesAdapter extends RecyclerView.Adapter<UsosViewHolder> {
+
+        private List<Usos> items = new ArrayList<>();
+
+        @Override public UsosViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.usos_row, parent, false);
+            return new UsosViewHolder(v);
+        }
+
+        @Override public void onBindViewHolder(UsosViewHolder holder, int position) {
+            Usos usos = items.get(position);
+            holder.name.setText(usos.name);
+            Picasso.with(getContext()).load(usos.logo).into(holder.logo);
+        }
+
+        @Override public int getItemCount() {
+            return items.size();
+        }
+
+        public void setItems(List<Usos> items) {
+            if (items != null) {
+                this.items = items;
+                notifyDataSetChanged();
+            }
+        }
+    }
+
+    static class UsosViewHolder extends RecyclerView.ViewHolder {
+
+        @Bind(R.id.usos_name) TextView name;
+        @Bind(R.id.usos_logo) ImageView logo;
+
+        public UsosViewHolder(View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+        }
     }
 }
