de67ed7c023a230c4ab55a5606d616bd286dad8f
Arkadiusz Konior <arkadiusz.konior@itako.pl>
2016-04-15 17:08:40 +0200

    bugfix

--- app/src/main/java/mobi/kujon/network/KujonBackendService.java
+++ app/src/main/java/mobi/kujon/network/KujonBackendService.java
@@ -1,5 +1,6 @@
 package mobi.kujon.network;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.auth.api.signin.GoogleSignInResult;
 
 import java.io.IOException;
@@ -53,10 +54,15 @@ public class KujonBackendService {
 
             Request originalRequest = chain.request();
             System.out.println("### modifying request" + originalRequest.url());
+            GoogleSignInAccount account = loginStatus.getSignInAccount();
+
+            String email = account != null ? account.getEmail() : "";
+            String token = account != null ? account.getIdToken() : "";
+
             Request request = originalRequest.newBuilder()
                     .header("Cookie", "KUJON_SECURE_COOKIE=\"2|1:0|10:1460710958|19:KUJON_SECURE_COOKIE|292:IntcImFjY2Vzc190b2tlbl9rZXlcIjogXCJZRXZqTEVZVTV2U2dkM1I5cHZ4dVwiLCBcIl9pZFwiOiB7XCIkb2lkXCI6IFwiNTcxMGExYjg4NTVlZjkyY2M5ZWE4MDExXCJ9LCBcImFjY2Vzc190b2tlbl9zZWNyZXRcIjogXCJzOUQ0Q2EyaE1mTTZiM2R3VHM1THJNYjZBaGFkZWpOOFZhNXZkR3JWXCIsIFwidXNvc19pZFwiOiBcIkRFTU9cIiwgXCJ1c29zX3BhaXJlZFwiOiB0cnVlfSI=|ac9226ebb15b7cb7b293e59ad5aa260a8032cba07822d522ccf9674fbbaaa169\"")
-                    .header("X-kujon.mobi-email", loginStatus.getSignInAccount().getEmail())
-                    .header("X-kujon.mobi-token", loginStatus.getSignInAccount().getIdToken())
+                    .header("X-kujon.mobi-email", email)
+                    .header("X-kujon.mobi-token", token)
                     .build();
 
             long t1 = System.nanoTime();
