53ab2e799465fd234a37250a77913d5eeb7b001a
Kacper Roszczyna <kacper.ros94@gmail.com>
2016-12-19 18:50:36 +0100

    added notifications enabling

--- app/src/main/java/mobi/kujon/activities/PreferencesActivity.java
+++ app/src/main/java/mobi/kujon/activities/PreferencesActivity.java
@@ -2,24 +2,107 @@ package mobi.kujon.activities;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.widget.SwitchCompat;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.TextView;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import mobi.kujon.R;
+import mobi.kujon.network.json.KujonResponse;
+import mobi.kujon.network.json.Preferences;
+import mobi.kujon.utils.ErrorHandlerUtil;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class PreferencesActivity extends BaseActivity {
 
     @Bind(R.id.toolbar_title) TextView toolbarTitle;
+    @Bind(R.id.notifications_enabler) SwitchCompat notificationsSwitch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activitiy_settings);
         ButterKnife.bind(this);
+        showProgress(true);
         toolbarTitle.setText(R.string.settings);
+        initSwitchStates();
+        initSwitchListeners();
+    }
+
+    private void initSwitchStates() {
+        kujonBackendApi.getUserPreferences().enqueue(new Callback<KujonResponse<Preferences>>() {
+            @Override
+            public void onResponse(Call<KujonResponse<Preferences>> call, Response<KujonResponse<Preferences>> response) {
+                showProgress(false);
+                if(ErrorHandlerUtil.handleResponse(response)) {
+                    notificationsSwitch.setChecked(response.body().data.notificationsEnabled);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<KujonResponse<Preferences>> call, Throwable t) {
+                showProgress(false);
+                ErrorHandlerUtil.handleError(t);
+            }
+        });
+    }
+
+    private void initSwitchListeners() {
+        notificationsSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                showProgress(true);
+                if (isChecked) {
+                    enableEvents();
+                } else {
+                    disableEvents();
+                }
+            }
+        });
+    }
+
+    private void enableEvents() {
+        kujonBackendApi.enableEvents().enqueue(new Callback<KujonResponse<Preferences>>() {
+            @Override
+            public void onResponse(Call<KujonResponse<Preferences>> call, Response<KujonResponse<Preferences>> response) {
+                showProgress(false);
+                if(ErrorHandlerUtil.handleResponse(response)) {
+                    showProgress(false);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<KujonResponse<Preferences>> call, Throwable t) {
+                showProgress(false);
+                notificationsSwitch.setChecked(false);
+                ErrorHandlerUtil.handleError(t);
+            }
+        });
+    }
+
+    private void disableEvents() {
+        showProgress(true);
+        kujonBackendApi.disableEvents().enqueue(new Callback<KujonResponse<Preferences>>() {
+            @Override
+            public void onResponse(Call<KujonResponse<Preferences>> call, Response<KujonResponse<Preferences>> response) {
+                showProgress(false);
+                if(ErrorHandlerUtil.handleResponse(response)) {
+                    showProgress(false);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<KujonResponse<Preferences>> call, Throwable t) {
+                showProgress(false);
+                notificationsSwitch.setChecked(true);
+                ErrorHandlerUtil.handleError(t);
+            }
+        });
     }
 
     @OnClick({R.id.logout, R.id.delete_account, R.id.regulations, R.id.contact_us, R.id.share_app})
---
+++ app/src/main/java/mobi/kujon/network/json/Preferences.java
@@ -0,0 +1,20 @@
+package mobi.kujon.network.json;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by kacper on 19.12.16.
+ */
+
+public class Preferences {
+
+    @SerializedName("events_enable")
+    @Expose
+    public Boolean notificationsEnabled;
+
+    @SerializedName("google_callendar_enable")
+    @Expose
+    public Boolean googleCalendarEnabled;
+
+}
