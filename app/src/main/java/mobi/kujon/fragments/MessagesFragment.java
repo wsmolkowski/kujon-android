a71c6b4656bd1e9ca7064072ab596c509c20ab94
Kacper Roszczyna <kacper.ros94@gmail.com>
2016-12-21 12:40:36 +0100

    added API classes

--- app/src/main/java/mobi/kujon/fragments/MessagesFragment.java
+++ app/src/main/java/mobi/kujon/fragments/MessagesFragment.java
@@ -1,7 +1,11 @@
 package mobi.kujon.fragments;
 
 
+import mobi.kujon.R;
+
 public class MessagesFragment extends ListFragment {
+
+
     @Override
     protected String getRequestUrl() {
         return null;
@@ -10,5 +14,11 @@ public class MessagesFragment extends ListFragment {
     @Override
     protected void loadData(boolean refresh) {
 
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        activity.setToolbarTitle(R.string.messages);
     }
 }
---
+++ app/src/main/java/mobi/kujon/network/json/Message.java
@@ -0,0 +1,25 @@
+package mobi.kujon.network.json;
+
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Message {
+
+    @SerializedName("created_time")
+    @Expose
+    public String createdTime;
+
+    @SerializedName("from")
+    @Expose
+    public String from;
+
+    @SerializedName("message")
+    @Expose
+    public String message;
+
+    @SerializedName("type")
+    @Expose
+    public String type;
+
+}
