dffdbebfc5a97acf782a23db88c1def117fdeab6
Kacper Roszczyna <kacper.ros94@gmail.com>
2016-12-21 12:18:35 +0100

    empty messages fragment

---
+++ app/src/main/java/mobi/kujon/fragments/MessagesFragment.java
@@ -0,0 +1,14 @@
+package mobi.kujon.fragments;
+
+
+public class MessagesFragment extends ListFragment {
+    @Override
+    protected String getRequestUrl() {
+        return null;
+    }
+
+    @Override
+    protected void loadData(boolean refresh) {
+
+    }
+}
