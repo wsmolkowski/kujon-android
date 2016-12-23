ba716a170e14e507c7a0f4f56863a63bfefa9a5a
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2016-12-23 11:51:13 +0100

    #140 layout imporovments

--- app/src/main/java/mobi/kujon/fragments/ThesesFragment.java
+++ app/src/main/java/mobi/kujon/fragments/ThesesFragment.java
@@ -37,24 +37,27 @@ public class ThesesFragment extends ListFragment {
 
     private Adapter adapter;
 
-    @Override public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         adapter = new Adapter();
         recyclerView.setAdapter(adapter);
         activity.showProgress(true);
 
-       loadData(false);
+        loadData(false);
     }
 
-    @Override protected String getRequestUrl() {
+    @Override
+    protected String getRequestUrl() {
         return backendApi.theses().request().url().toString();
     }
 
-    @Override protected void loadData(boolean refresh) {
-        if(getActivity().getIntent().getStringExtra(ThesesActivity.THESE_KEY) !=null){
+    @Override
+    protected void loadData(boolean refresh) {
+        if (getActivity().getIntent().getStringExtra(ThesesActivity.THESE_KEY) != null) {
             loadDataFromBundle();
 
-        }else {
+        } else {
             loadDataFromApi(refresh);
         }
 
@@ -62,28 +65,29 @@ public class ThesesFragment extends ListFragment {
 
     private void loadDataFromBundle() {
         String thesisJson = getActivity().getIntent().getStringExtra(ThesesActivity.THESE_KEY);
-        Gson gson  = new Gson();
+        Gson gson = new Gson();
         try {
             activity.showProgress(false);
             swipeContainer.setRefreshing(false);
             Thesis_ thesis = gson.fromJson(thesisJson, Thesis_.class);
             List<Thesis_> thesises = new ArrayList<>();
             thesises.add(thesis);
-            ((ThesesActivity)getActivity()).setToolbarTitle("Praca dyplomowe");
+            ((ThesesActivity) getActivity()).setToolbarTitle("Praca dyplomowe");
             adapter.setData(thesises);
-        }catch (JsonSyntaxException e){
+        } catch (JsonSyntaxException e) {
 
             activity.showProgress(false);
             swipeContainer.setRefreshing(false);
-            adapter.setData( new ArrayList<>());
+            adapter.setData(new ArrayList<>());
         }
     }
 
     private void loadDataFromApi(boolean refresh) {
-        ((ThesesActivity)getActivity()).setToolbarTitle("Prace dyplomowe");
+        ((ThesesActivity) getActivity()).setToolbarTitle("Prace dyplomowe");
         Call<KujonResponse<List<Thesis_>>> theses = refresh ? backendApi.thesesRefresh() : backendApi.theses();
         theses.enqueue(new Callback<KujonResponse<List<Thesis_>>>() {
-            @Override public void onResponse(Call<KujonResponse<List<Thesis_>>> call, Response<KujonResponse<List<Thesis_>>> response) {
+            @Override
+            public void onResponse(Call<KujonResponse<List<Thesis_>>> call, Response<KujonResponse<List<Thesis_>>> response) {
                 activity.showProgress(false);
                 swipeContainer.setRefreshing(false);
                 if (ErrorHandlerUtil.handleResponse(response)) {
@@ -92,7 +96,8 @@ public class ThesesFragment extends ListFragment {
                 }
             }
 
-            @Override public void onFailure(Call<KujonResponse<List<Thesis_>>> call, Throwable t) {
+            @Override
+            public void onFailure(Call<KujonResponse<List<Thesis_>>> call, Throwable t) {
                 activity.showProgress(false);
                 swipeContainer.setRefreshing(false);
                 ErrorHandlerUtil.handleError(t);
@@ -100,7 +105,8 @@ public class ThesesFragment extends ListFragment {
         });
     }
 
-    @Override public void onStart() {
+    @Override
+    public void onStart() {
         super.onStart();
     }
 
@@ -108,12 +114,14 @@ public class ThesesFragment extends ListFragment {
 
         List<Thesis_> data = new LinkedList<>();
 
-        @Override public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.row_thesis, parent, false);
             return new ViewHolder(v);
         }
 
-        @Override public void onBindViewHolder(ViewHolder holder, int position) {
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
             Thesis_ thesis = data.get(position);
             holder.title.setText(thesis.title);
             holder.type.setText(thesis.type);
@@ -123,12 +131,13 @@ public class ThesesFragment extends ListFragment {
             showList(activity.getLayoutInflater(), holder.supervisors, names, index -> showLecturerDatails(activity, thesis.supervisors.get(index).id));
 
             showList(activity.getLayoutInflater(), holder.faculty, Arrays.asList(thesis.faculty.name), index -> showFacultyDetails(activity, thesis.faculty.id));
-            if(position==getItemCount()-1){
+            if (position == getItemCount() - 1) {
                 holder.greyView.setVisibility(View.GONE);
             }
         }
 
-        @Override public int getItemCount() {
+        @Override
+        public int getItemCount() {
             return data.size();
         }
 
@@ -140,12 +149,19 @@ public class ThesesFragment extends ListFragment {
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
-        @Bind(R.id.title) TextView title;
-        @Bind(R.id.authors) TextView authors;
-        @Bind(R.id.supervisors) LinearLayout supervisors;
-        @Bind(R.id.type) TextView type;
-        @Bind(R.id.faculty) LinearLayout faculty;
-        @Bind(R.id.last_grey) View greyView;
+        @Bind(R.id.title)
+        TextView title;
+        @Bind(R.id.authors)
+        TextView authors;
+        @Bind(R.id.supervisors)
+        LinearLayout supervisors;
+        @Bind(R.id.type)
+        TextView type;
+        @Bind(R.id.faculty)
+        LinearLayout faculty;
+        @Bind(R.id.last_grey)
+        View greyView;
+
         public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
