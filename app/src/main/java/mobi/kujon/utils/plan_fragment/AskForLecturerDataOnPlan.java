f5de780432d1bb92b8d58ff902cab982d19b2b81
Kacper Roszczyna <kacper.ros94@gmail.com>
2017-01-09 12:58:54 +0100

    lecturer data on plan

---
+++ app/src/main/java/mobi/kujon/utils/plan_fragment/AskForLecturerDataOnPlan.java
@@ -0,0 +1,25 @@
+package mobi.kujon.utils.plan_fragment;
+
+
+import java.util.List;
+
+import mobi.kujon.network.KujonBackendApi;
+import mobi.kujon.network.json.CalendarEvent;
+import mobi.kujon.network.json.KujonResponse;
+import mobi.kujon.utils.KujonUtils;
+import retrofit2.Call;
+
+public class AskForLecturerDataOnPlan extends AskForDataOnPlan {
+
+    private String lecturerId;
+
+    public AskForLecturerDataOnPlan(ActivityChange activityChange, KujonUtils utils, KujonBackendApi backendApi, String lecturerId) {
+        super(activityChange, utils, backendApi);
+        this.lecturerId = lecturerId;
+    }
+
+    @Override
+    protected Call<KujonResponse<List<CalendarEvent>>> getKujonResponseCall(String restSuffix) {
+        return refresh ? backendApi.lecturerPlanRefresh(lecturerId, restSuffix) : backendApi.lecturerPlan(lecturerId, restSuffix);
+    }
+}
