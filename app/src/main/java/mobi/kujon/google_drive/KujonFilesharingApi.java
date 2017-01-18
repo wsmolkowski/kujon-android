c002053287537282f5b34425884573984adc9792
Kacper Roszczyna <kacper.ros94@gmail.com>
2017-01-18 12:49:12 +0100

    implemented according to newest documentation

--- app/src/main/java/mobi/kujon/google_drive/KujonFilesharingApi.java
+++ app/src/main/java/mobi/kujon/google_drive/KujonFilesharingApi.java
@@ -5,10 +5,12 @@ import java.util.List;
 
 import mobi.kujon.google_drive.model.KujonFile;
 import mobi.kujon.google_drive.model.ShareFileTarget;
+import mobi.kujon.google_drive.model.ShareFileTargetType;
 import mobi.kujon.google_drive.model.SharedFile;
 import mobi.kujon.google_drive.model.UploadedFile;
 import mobi.kujon.network.json.KujonResponse;
 import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.http.Body;
 import retrofit2.http.DELETE;
@@ -17,6 +19,7 @@ import retrofit2.http.Multipart;
 import retrofit2.http.POST;
 import retrofit2.http.Part;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 import retrofit2.http.Streaming;
 import rx.Observable;
 
@@ -25,8 +28,11 @@ public interface KujonFilesharingApi {
     @GET("files")
     Observable<KujonResponse<List<KujonFile>>> getAllFiles();
 
-    @GET("files/{termId}/{courseId}")
-    Observable<KujonResponse<List<KujonFile>>> getFiles(@Path("term_id") String termId, @Path("course_id") String courseId);
+    @GET("files")
+    Observable<KujonResponse<List<KujonFile>>> getFiles(@Query("course_id") String courseId, @Query("term_id") String termId);
+
+    @POST("filesshare")
+    Observable<KujonResponse<SharedFile>> shareFile(@Body ShareFileTarget shareFileTarget);
 
     @Streaming
     @GET("files/{fileId}")
@@ -39,10 +45,9 @@ public interface KujonFilesharingApi {
     @POST("filesupload")
     Observable<KujonResponse<UploadedFile>> uploadFile(@Part("coures_id") String courseId,
                                                        @Part("term_id") String termId,
-                                                       @Part("shared_user_usos_ids") List<String> sharedUserIds,
-                                                       @Part MultipartBody.Part file);
+                                                       @Part("file_share_with") @ShareFileTargetType.ShareFileTargetTypes String shareFileWith,
+                                                       @Part("share_list") List<Integer> shareTargets,
+                                                       @Part("files") RequestBody file);
 
-    @POST("filesshare")
-    Observable<KujonResponse<SharedFile>> shareFile(@Body ShareFileTarget shareFileTarget);
 
 }
