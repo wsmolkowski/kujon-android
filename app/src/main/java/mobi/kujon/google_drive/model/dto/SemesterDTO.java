9080ab68306f9b6e94e5c6070dd0b82d3dc08dcb
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-01-19 10:52:16 +0100

    create SubjectMVP classes

--- app/src/main/java/mobi/kujon/google_drive/model/dto/SemesterDTO.java
+++ app/src/main/java/mobi/kujon/google_drive/model/dto/SemesterDTO.java
@@ -30,4 +30,25 @@ public class SemesterDTO {
     public void setSemesterId(String semesterId) {
         this.semesterId = semesterId;
     }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof SemesterDTO)) return false;
+
+        SemesterDTO that = (SemesterDTO) o;
+
+        if (semesterCode != null ? !semesterCode.equals(that.semesterCode) : that.semesterCode != null)
+            return false;
+        return semesterId != null ? semesterId.equals(that.semesterId) : that.semesterId == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = semesterCode != null ? semesterCode.hashCode() : 0;
+        result = 31 * result + (semesterId != null ? semesterId.hashCode() : 0);
+        return result;
+    }
 }
