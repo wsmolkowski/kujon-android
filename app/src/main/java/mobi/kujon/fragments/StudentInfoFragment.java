fb6b9a737076713280c10788782a15fd24e87c7c
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-01-30 10:34:40 +0100

    fix crash on showing Kierunek from Student Info fragment

--- app/src/main/java/mobi/kujon/fragments/StudentInfoFragment.java
+++ app/src/main/java/mobi/kujon/fragments/StudentInfoFragment.java
@@ -30,7 +30,6 @@ import mobi.kujon.activities.StudentDetailsActivity;
 import mobi.kujon.network.KujonBackendApi;
 import mobi.kujon.network.json.KujonResponse;
 import mobi.kujon.network.json.Programme;
-import mobi.kujon.network.json.ProgrammeSingle;
 import mobi.kujon.network.json.StudentProgramme;
 import mobi.kujon.network.json.User;
 import mobi.kujon.network.json.Usos;
@@ -123,23 +122,7 @@ public class StudentInfoFragment extends BaseFragment {
                         activity.showProgress(true);
                         StudentProgramme studentProgramme = user.student_programmes.get(position);
                         Programme programme = studentProgramme.programme;
-                        kujonBackendApi.programmes(programme.id).enqueue(new Callback<KujonResponse<ProgrammeSingle>>() {
-                            @Override
-                            public void onResponse(Call<KujonResponse<ProgrammeSingle>> call, Response<KujonResponse<ProgrammeSingle>> response) {
-                                activity.showProgress(false);
-                                if (ErrorHandlerUtil.handleResponse(response)) {
-                                    ProgrammeSingle prog = response.body().data;
-                                    String name = !isEmpty(prog.name) ? prog.name.split(",")[0] : "";
-                                    ProgrammeDetailsActivity.showProgrammeDetails(StudentInfoFragment.this.getActivity(), prog, name);
-                                }
-                            }
-
-                            @Override
-                            public void onFailure(Call<KujonResponse<ProgrammeSingle>> call, Throwable t) {
-                                activity.showProgress(false);
-                                ErrorHandlerUtil.handleError(t);
-                            }
-                        });
+                        ProgrammeDetailsActivity.showProgrammeDetailsWithLoad(StudentInfoFragment.this.getActivity(), programme.id);
                     });
                 }
             }
