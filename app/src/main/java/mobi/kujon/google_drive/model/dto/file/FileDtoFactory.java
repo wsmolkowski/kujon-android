6c221450e013a8f650f0d2479ef0ae54048345f1
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-02-02 12:24:56 +0100

    basic refactor

--- app/src/main/java/mobi/kujon/google_drive/model/dto/file/FileDtoFactory.java
+++ app/src/main/java/mobi/kujon/google_drive/model/dto/file/FileDtoFactory.java
@@ -11,22 +11,30 @@ import mobi.kujon.google_drive.model.KujonFile;
 
 public class FileDtoFactory {
 
+    private static final String IMAGE_CONTENT_FILE = "image";
+    private static final String VIDEO_CONTENT_FILE = "video";
+    private static final String AUDIO_CONTENT_FILE = "audio";
+    private static final String PDF_FILE_CONENT = "pdf";
+    private static final String DOC_FILE_CONTENT = "document";
+    private static final String PPT_FILE_CONTENT = "presentation";
+    private static final String XLS_FILE_CONTENT = "sheet";
+
     public static FileDTO createFileDto(KujonFile kujonFile){
         String contentType = kujonFile.contentType;
 
-        if(contentType.startsWith("image")){
+        if(contentType.startsWith(IMAGE_CONTENT_FILE)){
             return new ImageFileType(kujonFile);
-        }else if(contentType.startsWith("video")){
+        }else if(contentType.startsWith(VIDEO_CONTENT_FILE)){
             return new VideoFileType(kujonFile);
-        }else if(contentType.startsWith("audio")) {
+        }else if(contentType.startsWith(AUDIO_CONTENT_FILE)) {
             return new AudioFileType(kujonFile);
-        }else if(contentType.contains("pdf")) {
+        }else if(contentType.contains(PDF_FILE_CONENT)) {
             return new PdfFileType(kujonFile);
-        }else if(contentType.contains("document")) {
+        }else if(contentType.contains(DOC_FILE_CONTENT)) {
             return new DocFileType(kujonFile);
-        }else if(contentType.contains("presentation")) {
+        }else if(contentType.contains(PPT_FILE_CONTENT)) {
             return new PptFileType(kujonFile);
-        }if(contentType.contains("sheet")) {
+        }if(contentType.contains(XLS_FILE_CONTENT)) {
             return new XlsFileType(kujonFile);
         }else {
             return new NormalFileType(kujonFile);
@@ -41,8 +49,3 @@ public class FileDtoFactory {
         return fileDTOs;
     }
 }
-
-//
-//case msWord = "application/vnd.openxmlformats-officedocument.wordprocessingml.document"
-//        case msPowerPoint = "application/vnd.openxmlformats-officedocument.presentationml.presentation"
-//        case msExcel = "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
