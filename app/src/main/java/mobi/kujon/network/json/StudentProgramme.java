ed51f3cf2fea8d1a29d29145ad0ab9c19109526f
wojciechmaciejewski <wojciechmaciejewski1985@ga>
2017-03-02 12:12:34 +0100

    add some icons and text to StudentProgrammes

--- app/src/main/java/mobi/kujon/network/json/StudentProgramme.java
+++ app/src/main/java/mobi/kujon/network/json/StudentProgramme.java
@@ -1,7 +1,13 @@
 package mobi.kujon.network.json;
 
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
+
 import com.google.gson.annotations.Expose;
 import com.google.gson.annotations.SerializedName;
+
+import mobi.kujon.R;
+import mobi.kujon.utils.programme_status.StudentProgrammeStatus;
 
 public class StudentProgramme {
 
@@ -12,5 +18,46 @@ public class StudentProgramme {
     @SerializedName("programme")
     @Expose
     public Programme programme;
+
+
+    @Expose
+    public @StudentProgrammeStatus  String status;
+
+
+
+    public @StringRes int getGraduateText(){
+        switch (status){
+            case StudentProgrammeStatus.CANCELLED:
+                return R.string.cancelled_study;
+            case StudentProgrammeStatus.ACTIVE:
+                return R.string.active_study;
+            case StudentProgrammeStatus.GRADUATE_END_OF_STUDY:
+                return R.string.graduated_end_of_study;
+            case StudentProgrammeStatus.GRADUATE_BEFORE_DIPLOMA:
+                return R.string.graduated_before;
+            case StudentProgrammeStatus.GRADUATE_DIPLOMA:
+                return R.string.graduated_diploma;
+            default:
+                return R.string.active_study;
+        }
+    }
+
+    public @DrawableRes int getImage(){
+        switch (status){
+            case StudentProgrammeStatus.CANCELLED:
+                return R.drawable.ic_canceled_study;
+            case StudentProgrammeStatus.ACTIVE:
+                return R.drawable.ic_active_study;
+            case StudentProgrammeStatus.GRADUATE_END_OF_STUDY:
+                return R.drawable.ic_graduated_end_of_study;
+            case StudentProgrammeStatus.GRADUATE_BEFORE_DIPLOMA:
+                return R.drawable.ic_graduated_before_diploma;
+            case StudentProgrammeStatus.GRADUATE_DIPLOMA:
+                return R.drawable.ic_graduated_diploma;
+            default:
+                return R.drawable.ic_active_study;
+        }
+    }
+
 
 }
---
+++ app/src/main/java/mobi/kujon/utils/programme_status/StudentProgrammeStatus.java
@@ -0,0 +1,20 @@
+package mobi.kujon.utils.programme_status;
+
+import android.support.annotation.StringDef;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+/**
+ *
+ */
+@StringDef({StudentProgrammeStatus.ACTIVE,StudentProgrammeStatus.CANCELLED,StudentProgrammeStatus.GRADUATE_DIPLOMA,StudentProgrammeStatus.GRADUATE_BEFORE_DIPLOMA,StudentProgrammeStatus.GRADUATE_END_OF_STUDY})
+@Retention(RetentionPolicy.SOURCE)
+public @interface StudentProgrammeStatus {
+    String CANCELLED = "cancelled";
+    String ACTIVE  =  "active";
+    String GRADUATE_END_OF_STUDY = "graduated_end_of_study";
+    String GRADUATE_BEFORE_DIPLOMA = "graduated_before_diploma";
+    String GRADUATE_DIPLOMA = "graduated_diploma";
+
+}
